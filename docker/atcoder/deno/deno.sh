f9c242c8eb263c3a09f22556fcbb55177760d0e6
malleroid <38337195+malleroid@users.noreply.github.com>
2026-04-06 21:21:41 +0900

    ✨ add AtCoder JavaScript (Deno 2.4.5) Dockerfile
    Reproduce the AtCoder 2025/10 judge environment for JavaScript (Deno) locally.
    41 Deno standard library packages and 6 npm libraries pre-installed.
    Includes deno.sh wrapper script for V8 stack size limiting.

---
+++ docker/atcoder/deno/deno.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [ "$1" -gt 1048576 ]; then
+  stack_size=1048576
+else
+  stack_size="$1"
+fi
+
+deno \
+run \
+--allow-all \
+--quiet \
+--no-prompt \
+--v8-flags=--stack-size="$stack_size" \
+Main.js \
+ONLINE_JUDGE \
+ATCODER
